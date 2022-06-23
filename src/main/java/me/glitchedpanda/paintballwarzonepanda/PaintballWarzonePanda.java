c49c25a510b3ecfe653fd0ed6367353c600c1d4b
Glitched Panda <blablabla12345y789@gmail.com>
2022-06-23 13:15:12 +0200

    Initial Commit

--- src/main/java/me/glitchedpanda/paintballwarzonepanda/PaintballWarzonePanda.java
+++ src/main/java/me/glitchedpanda/paintballwarzonepanda/PaintballWarzonePanda.java
@@ -1,6 +1,6 @@
 package me.glitchedpanda.paintballwarzonepanda;
 
-import me.glitchedpanda.paintballwarzone.painballguns.TeamFrost;
+import me.glitchedpanda.paintballwarzonepanda.paintballguns.TeamFrost;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
