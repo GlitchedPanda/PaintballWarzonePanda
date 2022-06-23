adaf155b8ffa1d51108ca8a62d72fe23beba7d06
Glitched Panda <blablabla12345y789@gmail.com>
2022-06-23 15:38:14 +0200

    Commit 8

--- src/main/java/me/glitchedpanda/paintballwarzonepanda/items/PaintballGun.java
+++ src/main/java/me/glitchedpanda/paintballwarzonepanda/items/PaintballGun.java
@@ -54,6 +54,7 @@ public class PaintballGun implements Listener {
 
     @EventHandler
     public void onRightCLick(PlayerInteractEvent e) {
+        assert this instanceof PaintballGun;
         if (e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
             shoot(e.getPlayer());
         } else if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
